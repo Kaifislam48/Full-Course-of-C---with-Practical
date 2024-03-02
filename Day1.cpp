d452d6284aa27c6b27d2bd116c498ab8e779177b
kaif <kaifislam48@gmail.com>
2024-03-02 17:15:41 +0530

    first commit

---
+++ Day1.cpp
@@ -0,0 +1,24 @@
+#include<iostream>   //header file library in which i/p and o/p file are include.
+using namespace std;  //use names for objects and variables from the standard library.
+
+int main(){   //main function code running start from here.
+    cout<<"Hello World!";  // cout is use for Printint Output.
+    return 0;  //return 0 ends the main function its optional in c++ use or not.
+}
+
+/*
+//--->This symbol is use for single line comment. Comment means which is not part of code its use for only code readable purpose and nothing else.
+*/
+
+// /* */-->This symbol is use for multi line Comment.
+
+/*
+//We can also write on this way.
+
+#include<iostream>
+
+int main(){
+    std::cout<<"Hello World!";  
+    return 0;  
+}
+*/
